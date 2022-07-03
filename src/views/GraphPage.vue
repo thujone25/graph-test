0bbd9e44ad7e2a1e3cab4299f4267eb2afd22d81
Bohdan Steblovskyi <junkbox@email.ua>
2022-07-03 20:25:40 +0300

    Fix lint

--- src/views/GraphPage.vue
+++ src/views/GraphPage.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="graph-page">
     <header class="graph-page__header">
-      <button class="g-button graph-page__btn" @click="saveGraph">Save graph!</button>
+      <button class="g-button graph-page__btn" @click="saveGraph">
+        Save graph!
+      </button>
       <button class="g-button graph-page__btn" @click="logout">Logout</button>
     </header>
     <div ref="graphPaper" class="graph-page__graph-paper"></div>
@@ -21,48 +23,47 @@ export default {
       graph: null,
       portTemplate: {
         position: {
-          name: 'left'
+          name: "left",
         },
-        attrs: { 
-          portBody: { 
+        attrs: {
+          portBody: {
             magnet: true,
             r: 6,
-            fill: '#E6A502',
-            stroke:'#023047'
-          }
+            fill: "#E6A502",
+            stroke: "#023047",
+          },
         },
-        markup: [{
-          tagName: 'circle',
-          selector: 'portBody'
-        }]
-      }
-    }
+        markup: [
+          {
+            tagName: "circle",
+            selector: "portBody",
+          },
+        ],
+      },
+    };
   },
   methods: {
     saveGraph() {
       const elems = this.graph.getElements();
-      elems.forEach(e => {
-        if (e.id === this.templateRectId || e.get('locked')) {
+      elems.forEach((e) => {
+        if (e.id === this.templateRectId || e.get("locked")) {
           e.remove();
         }
-      })
+      });
       const graph = JSON.stringify(this.graph.toJSON());
       localStorage.setItem("app_saved_graph", graph);
       this.createSidebar();
     },
     logout() {
       Logout();
-      this.$router.push('/');
+      this.$router.push("/");
     },
     getPorts() {
       const leftPort = JSON.parse(JSON.stringify(this.portTemplate));
       const rightPort = JSON.parse(JSON.stringify(this.portTemplate));
-      leftPort.position.name = 'left';
-      rightPort.position.name = 'right';
-      return [
-        leftPort,
-        rightPort,
-      ]
+      leftPort.position.name = "left";
+      rightPort.position.name = "right";
+      return [leftPort, rightPort];
     },
     createTemplateRect() {
       const ports = this.getPorts();
@@ -73,17 +74,17 @@ export default {
           groups: {
             left: ports[0],
             right: ports[1],
-          }
+          },
         },
         attrs: {
           body: {
-            fill: 'blue'
+            fill: "blue",
           },
           label: {
-            text: 'Template elem',
-            fill: 'white'
+            text: "Template elem",
+            fill: "white",
           },
-        }
+        },
       });
       this.templateRectId = rect.id;
       rect.addTo(this.graph);
@@ -93,8 +94,8 @@ export default {
       parentRect.resize(180, 74);
       parentRect.attr({
         body: {
-            fill: 'pink'
-        }
+          fill: "pink",
+        },
       });
       this.parents.push(parentRect.id);
       return parentRect;
@@ -106,15 +107,16 @@ export default {
       const elemPosY = elModel.position().y;
       const siblingPosY = sibling.position().y;
       const indexOfSibling = cells.indexOf(sibling);
-      const newIndex = elemPosY <= siblingPosY ? indexOfSibling : indexOfSibling + 1;
+      const newIndex =
+        elemPosY <= siblingPosY ? indexOfSibling : indexOfSibling + 1;
       const newOrderArr = [...cells];
       newOrderArr.splice(newIndex, 0, elModel);
-      newOrderArr.forEach(i => {
+      newOrderArr.forEach((i) => {
         if (i.isEmbeddedIn(parent)) parent.unembed(i);
-      })
-      newOrderArr.forEach(i => {
+      });
+      newOrderArr.forEach((i) => {
         parent.embed(i);
-      })
+      });
       this.handleChildrenPos(parent);
     },
     manageParent(el) {
@@ -143,10 +145,10 @@ export default {
       const allElems = this.graph.getElements();
       for (const cell of allElems) {
         if (
-            el.getBBox().intersect(cell.getBBox()) &&
-            !this.isParentEl(cell) &&
-            cell.id !== el.model.id
-          ) {
+          el.getBBox().intersect(cell.getBBox()) &&
+          !this.isParentEl(cell) &&
+          cell.id !== el.model.id
+        ) {
           return cell;
         }
       }
@@ -155,29 +157,26 @@ export default {
       const parentPos = parent.position();
       const constX = parentPos.x + 15;
       const cells = parent.getEmbeddedCells();
-      parent.fitEmbeds({padding: 15});
+      parent.fitEmbeds({ padding: 15 });
       for (let i = 0, lim = cells.length; i < lim; i += 1) {
         const cell = cells[i];
-        const yPos = parentPos.y + 15 + (44 * i) + (i ? (8 * i) : 0);
+        const yPos = parentPos.y + 15 + 44 * i + (i ? 8 * i : 0);
         cell.position(constX, yPos);
       }
-      parent.fitEmbeds({padding: 15});
+      parent.fitEmbeds({ padding: 15 });
     },
     onElPointerUp(el) {
       const currentElement = el.model;
-      const isTemplateElem = currentElement.id === this.templateRectId
+      const isTemplateElem = currentElement.id === this.templateRectId;
       if (currentElement.position()?.x > 180) {
         if (isTemplateElem) {
           this.elementsCounter += 1;
           currentElement.attr({
             label: {
-                text: `Element #${this.elementsCounter}`
-            }
+              text: `Element #${this.elementsCounter}`,
+            },
           });
-          currentElement.addPorts([
-            {group: 'left'},
-            {group: 'right'},
-          ]);
+          currentElement.addPorts([{ group: "left" }, { group: "right" }]);
           this.createTemplateRect();
         }
         if (!this.isParentEl(el)) this.manageParent(el);
@@ -185,7 +184,8 @@ export default {
         if (isTemplateElem) {
           currentElement.position(15, 15);
         } else {
-          if (this.isParentEl(currentElement)) this.removeParentID(currentElement.id);
+          if (this.isParentEl(currentElement))
+            this.removeParentID(currentElement.id);
           currentElement.remove();
         }
       }
@@ -196,7 +196,7 @@ export default {
     },
     watchAllParents() {
       const elems = this.graph.getCells();
-      elems.forEach(i => {
+      elems.forEach((i) => {
         if (this.isParentEl(i)) {
           if (i.getEmbeddedCells()?.length) {
             this.handleChildrenPos(i);
@@ -205,34 +205,34 @@ export default {
             i.remove();
           }
         }
-      })
+      });
     },
     parseSavedGraph() {
       this.graph.fromJSON(JSON.parse(localStorage.getItem("app_saved_graph")));
       const elems = this.graph.getElements();
-      elems.forEach(e => {
+      elems.forEach((e) => {
         if (e.getEmbeddedCells().length) {
           this.parents.push(e.id);
         }
-      })
+      });
     },
     createSidebar() {
       const sidebarRect = new this.j.shapes.standard.Rectangle();
-      sidebarRect.set('locked', true);
+      sidebarRect.set("locked", true);
       sidebarRect.position(0, 0);
       sidebarRect.resize(180, 1000);
       sidebarRect.attr({
-          body: {
-              fill: 'yellow'
-          },
-          label: {
-              text: 'Drop here to remove',
-              fill: 'black'
-          }
+        body: {
+          fill: "yellow",
+        },
+        label: {
+          text: "Drop here to remove",
+          fill: "black",
+        },
       });
       sidebarRect.addTo(this.graph);
       this.createTemplateRect();
-    }
+    },
   },
   mounted() {
     this.j = window.joint;
@@ -242,34 +242,35 @@ export default {
       const paper = new this.j.dia.Paper({
         el: this.$refs.graphPaper,
         model: this.graph,
-        width: '100%',
-        height: '100%',
+        width: "100%",
+        height: "100%",
         gridSize: 1,
         cellViewNamespace: namespace,
         linkPinning: false,
         embeddingMode: true,
         validateEmbedding: (childView, parentView) => {
-          if (this.isParentEl(parentView) && !this.isParentEl(childView)) return true;
+          if (this.isParentEl(parentView) && !this.isParentEl(childView))
+            return true;
           return false;
         },
         interactive: (cellView) => {
-          if (cellView.model.get('locked')) {
+          if (cellView.model.get("locked")) {
             return {
-              elementMove: false
+              elementMove: false,
             };
           }
           return true;
-        }
+        },
       });
-      paper.on('element:pointerup', (el) => {
-        if (!el.model.get('locked')) this.onElPointerUp(el);
+      paper.on("element:pointerup", (el) => {
+        if (!el.model.get("locked")) this.onElPointerUp(el);
       });
       if (localStorage.getItem("app_saved_graph")) {
         this.parseSavedGraph();
       }
       this.createSidebar();
     }
-  }
+  },
 };
 </script>
 
